9de85a17b32e2e90fa16a065ab09c740200adbfc
nicollaspiettro08 <damattanicollaspiettro@gmail.com>
2025-04-03 11:30:28 -0400

    atividades

---
+++ atividade-01.js
@@ -0,0 +1,22 @@
+let nome = 'Nícollas Piettro';
+let idade = 15;
+console.log('Olá meu nome é',nome,'e eu tenho',idade,'anos');
+
+// atividade 2
+
+let st= 'string';
+console.log ( st.toLowerCase()); 
+console.log ( st.toUpperCase()); 
+
+// atividade 3 
+
+let num = 150;
+console.log(typeof num);
+
+// atividade 4
+
+let alt = 1.85;
+let pes = 75;
+let imc = pes/(alt*alt);
+imc.toFixed()
+console.log(imc);
---
+++ atividae-02.js
@@ -0,0 +1,3 @@
+let st= 'string';
+console.log ( st.toLowerCase()); 
+console.log ( st.toUpperCase()); 
